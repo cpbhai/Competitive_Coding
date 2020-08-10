0c02603f06900c4f0d034832c220d2a0a3392aee
Harshvardhan Singh <51012630+harshvcode@users.noreply.github.com>
2020-08-10 18:00:28 +0530

    Update MedianofTwoSortedArrays.cpp

--- Leetcode/MedianofTwoSortedArrays.cpp
+++ Leetcode/MedianofTwoSortedArrays.cpp
@@ -5,7 +5,7 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        nums1.insert(nums1.begin(), nums2.begin(), nums2.end());
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
         sort(nums1.begin(), nums1.end());
         int n=(int) nums1.size();
         return (n % 2 == 0) ? (double)(nums1[n/2]+nums1[(n/2)-1])/2:(double)nums1[n/2];
